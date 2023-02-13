2f474a6f94e4d4139a5c2c63e8008b7e2a37b81a
Florent Jaouali <florent.jaouali@gmail.com>
2023-02-13 11:35:48 +0100

    Update for API test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token, if: :api_request_authorized?
   before_action :authenticate_user!
 
+  def api_request_authorized?
+    if request.headers["request-is-api"] &&  request.headers["request-is-api"] == "7d160f9c-6ac6-49f3-9a34-058a32b41015"
+      return true
+    else
+      return false
+    end
+  end
+  
 end
